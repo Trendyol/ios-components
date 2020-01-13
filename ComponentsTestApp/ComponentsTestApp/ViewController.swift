f6df9f7a294d76c2c47bd617ec43623f90686e1d
Emre <ergunemr@itu.edu.tr>
2020-01-13 15:08:03 +0300

    Create test project

---
+++ ComponentsTestApp/ComponentsTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComponentsTestApp
+//
+//  Created by Emre Ergün on 13.01.2020.
+//  Copyright © 2020 Emre Ergün. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
