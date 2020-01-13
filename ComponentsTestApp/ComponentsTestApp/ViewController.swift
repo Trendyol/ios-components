5fe7f2a6ce500885362ace399afd9d6fb561e3e4
Emre <ergunemr@itu.edu.tr>
2020-01-13 15:20:36 +0300

    Prepare and show imageslider with test urls.

--- ComponentsTestApp/ComponentsTestApp/ViewController.swift
+++ ComponentsTestApp/ComponentsTestApp/ViewController.swift
@@ -12,11 +12,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet private weak var imageSlider: ImageSliderView!
+    
+    
+    let testImageUrls = [
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_20425452.jpg",
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_20425453.jpg",
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_20425454.jpg",
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_123070377.jpg",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        prepareImageSlider()
+    }
+    
+    private func prepareImageSlider() {
+        let imageSliderPresenter = ImageSliderViewPresenter(imageUrls: testImageUrls, view: imageSlider)
+        imageSlider.presenter = imageSliderPresenter
     }
 }
 
