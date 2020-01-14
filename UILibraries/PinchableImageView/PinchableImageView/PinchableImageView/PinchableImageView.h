378fd88d08a9cc25dbab642912b1647a05477c4d
Emre <ergunemr@itu.edu.tr>
2020-01-14 09:27:34 +0300

    Create pinchable image view framework under ios components

---
+++ UILibraries/PinchableImageView/PinchableImageView/PinchableImageView/PinchableImageView.h
@@ -0,0 +1,19 @@
+//
+//  PinchableImageView.h
+//  PinchableImageView
+//
+//  Created by Emre Ergün on 14.01.2020.
+//  Copyright © 2020 Emre Ergün. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinchableImageView.
+FOUNDATION_EXPORT double PinchableImageViewVersionNumber;
+
+//! Project version string for PinchableImageView.
+FOUNDATION_EXPORT const unsigned char PinchableImageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinchableImageView/PublicHeader.h>
+
+
