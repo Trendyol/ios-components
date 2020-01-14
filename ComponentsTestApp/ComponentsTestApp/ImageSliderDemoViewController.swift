c000cd307b5cc8b6ace00a7949ef46ec3c023a64
Emre <ergunemr@itu.edu.tr>
2020-01-14 15:36:50 +0300

    Implement basic functionality of image slider framework demo

--- ComponentsTestApp/ComponentsTestApp/ImageSliderDemoViewController.swift
+++ ComponentsTestApp/ComponentsTestApp/ImageSliderDemoViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2020 Emre Ergün. All rights reserved.
 //
 
+import ImageSlider
 import UIKit
 
 final class ImageSliderDemoViewController: UIViewController {
+    
+    let imageUrls = [
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_20425452.jpg",
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_20425453.jpg",
+    "https://cdn.dolap.com/product/org/canta/kol-cantasi/herry_123070377.jpg"
+    ]
+    
+    @IBOutlet private weak var imageSliderView: ImageSliderView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Image Slider"
+        
+        prepareImageSlider()
+    }
+    
+    private func prepareImageSlider() {
+        let imageSliderPresenter = ImageSliderViewPresenter(imageUrls: imageUrls,
+                                                            loopingEnabled: true,
+                                                            view: imageSliderView)
+        imageSliderView.presenter = imageSliderPresenter
     }
 }
