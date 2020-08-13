40bbaa95eb4dfa436aa170880ab596026d80520e
Mustafa GUNES <gunes149@gmail.com>
2020-08-13 12:01:45 +0300

    PropertyWrapper improvements
        - Swiftlint added to build phases
        - PropertyWrapper code optimization done

--- Utils/PropertyWrapper/PropertyWrapper/Classes/DefaultCodable.swift
+++ Utils/PropertyWrapper/PropertyWrapper/Classes/DefaultCodable.swift
@@ -8,23 +8,23 @@
 
 public protocol DefaultCodableInterface {
     associatedtype RawValue: Codable
-    
+
     static var defaultValue: RawValue { get }
 }
 
 @propertyWrapper
 public struct DefaultCodable<T: DefaultCodableInterface>: Codable {
     public var wrappedValue: T.RawValue
-    
+
     public init(wrappedValue: T.RawValue) {
         self.wrappedValue = wrappedValue
     }
-    
+
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         self.wrappedValue = (try? container.decode(T.RawValue.self)) ?? T.defaultValue
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         try wrappedValue.encode(to: encoder)
     }
@@ -32,4 +32,3 @@ public struct DefaultCodable<T: DefaultCodableInterface>: Codable {
 
 extension DefaultCodable: Equatable where T.RawValue: Equatable { }
 extension DefaultCodable: Hashable where T.RawValue: Hashable { }
-
--- Utils/PropertyWrapper/PropertyWrapperTests/PropertyWrapperTests.swift
+++ Utils/PropertyWrapper/PropertyWrapperTests/PropertyWrapperTests.swift
@@ -9,34 +9,38 @@
 import XCTest
 @testable import PropertyWrapper
 
+// swiftlint:disable force_try
+// swiftlint:disable force_unwrapping
 final class PropertyWrapperTests: XCTestCase {
     func test_AdminType_ShouldBeGivenTypeSelected() {
         let jsonData = #"{ "user": "admin" }"#.data(using: .utf8)!
         let response = try! JSONDecoder().decode(UserResponse.self, from: jsonData)
         XCTAssertEqual(response.user, UserType.admin)
     }
-    
+
     func test_FailableUserType_ShouldBeLastCaseSelected() {
         let jsonData = #"{ "user": "NewTypeUser" }"#.data(using: .utf8)!
         let response = try! JSONDecoder().decode(UserResponse.self, from: jsonData)
         XCTAssertEqual(response.user, UserType.none)
     }
-    
+
     func test_NewStatusAdded_ShouldBeFirstCaseSelected() {
         let jsonData = #"{ "status": "newStatusAdded" }"#.data(using: .utf8)!
         let response = try! JSONDecoder().decode(OrderResponse.self, from: jsonData)
         XCTAssertEqual(response.status, OrderStatus.new)
     }
-    
+
     func test_EmptyFieldOnOrderResponse_ShouldShowOrderStatusShouldBeTrue() {
         let jsonData = #"{ }"#.data(using: .utf8)!
         let response = try! JSONDecoder().decode(OrderResponse.self, from: jsonData)
         XCTAssertTrue(response.shouldShowOrderStatus)
     }
-    
+
     func test_EmptyFieldOnUserResponse_IsAdminShouldBeFalse() {
         let jsonData = #"{ }"#.data(using: .utf8)!
         let response = try! JSONDecoder().decode(UserResponse.self, from: jsonData)
         XCTAssertFalse(response.isAdmin)
     }
 }
+// swiftlint:enable force_try
+// swiftlint:enable force_unwrapping
